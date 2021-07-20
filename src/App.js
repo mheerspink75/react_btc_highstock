04b5f0ebd995211c401d1edea5fa04f91581bc7b
Matt Heerspink <mheerspink75@gmail.com>
2021-07-20 01:24:51 -0500

    added linearGradient and price fetching to app.js

--- src/App.js
+++ src/App.js
@@ -3,49 +3,81 @@ import { useState, useEffect } from "react";
 import Highcharts from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
 
-let symbol = "BTC";
+let symbol = ["BTC"];
 
+// Get time series data
 let dataUrl =
   "https://min-api.cryptocompare.com/data/v2/histoday?fsym=" +
   symbol +
   "&tsym=USD&limit=400";
 
+// Get current price
+let priceUrl =
+  "https://min-api.cryptocompare.com/data/price?fsym=" + symbol + "&tsyms=USD";
+
 function App() {
   const [appState, setAppState] = useState({
     loading: false,
     data: null,
+    price: null,
   });
 
   useEffect(() => {
     setAppState({ loading: true });
     let arr = [];
-
-    fetch(dataUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        for (const key of data.Data.Data) {
-          let data = [key.time * 1000, key.close];
-          arr.push(data);
-        }
-        setAppState({ loading: false, data: arr });
+    let arr1 = [];
+    // Get price
+    fetch(priceUrl)
+      .then((response) => response.json())
+      .then((data1) => {
+        let price = data1.USD;
+        arr1.push(price);
+        // Get time-series
+        fetch(dataUrl)
+          .then((res) => res.json())
+          .then((data) => {
+            for (const key of data.Data.Data) {
+              let data = [key.time * 1000, key.close];
+              arr.push(data);
+            }
+            setAppState({ loading: false, data: arr, price: arr1 });
+          });
       });
   }, [setAppState]);
 
   let data = appState.data;
+  let price = appState.price;
+  
 
   const options = {
     chart: {
       backgroundColor: "white",
-      type: "line",
-      height: "700"
+      type: "area",
+      height: "520px",
     },
     title: {
-      text: symbol,
+      text: `<h1 id="chart-title">${symbol} $${price}</h1>`,
       align: "left",
+    },
+    plotOptions: {
+      series: {
+        fillColor: {
+          linearGradient: [0, 0, 0, 300],
+          stops: [
+            [0, Highcharts.getOptions().colors[0]],
+            [
+              1,
+              Highcharts.color(Highcharts.getOptions().colors[0])
+                .setOpacity(0)
+                .get("rgba"),
+            ],
+          ],
+        },
+      },
     },
     series: [
       {
-        name: symbol,
+        name: symbol.value,
         data: data,
         tooltip: {
           valueDecimals: 2,
@@ -61,7 +93,6 @@ function App() {
         constructorType={"stockChart"}
         options={options}
       />
-      <div id="message">{appState.message}</div>
     </div>
   );
 }
